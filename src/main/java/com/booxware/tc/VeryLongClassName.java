d1e9f0624f07a03093095a7a2792bdaa5b0ca1f6
Krzysztof Mirek <krzysztof.mirek@booxware.de>
2016-03-31 15:55:30 +0200

    initial commit

---
+++ src/main/java/com/booxware/tc/VeryLongClassName.java
@@ -0,0 +1,16 @@
+package com.booxware.tc;
+
+
+public class VeryLongClassName
+{
+    public static void main( String[] args )
+    {
+
+        boolean x = (byte) 1 == foo();
+        System.out.println(x);
+    }
+
+    private static Byte foo () {
+        return null;
+    }
+}
---
+++ src/test/java/com/booxware/tc/VeryLongClassNameTest.java
@@ -0,0 +1,16 @@
+package com.booxware.tc;
+
+import org.junit.Test;
+
+public class VeryLongClassNameTest {
+
+    @Test
+    public void testA() {
+        System.out.println("test A");
+    }
+
+    @Test
+    public void testB() {
+        System.out.println("test B");
+    }
+}
