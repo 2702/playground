c3a3b905dd54c4ce62a5ffbbfe89c33519a5e97e
Krzysztof Mirek <krzysztof.mirek@booxware.de>
2016-03-31 16:16:23 +0200

    cleanup

--- src/main/java/com/booxware/tc/VeryLongClassName.java
+++ src/main/java/com/booxware/tc/VeryLongClassName.java
@@ -5,9 +5,7 @@ public class VeryLongClassName
 {
     public static void main( String[] args )
     {
-
-        boolean x = (byte) 1 == foo();
-        System.out.println(x);
+        System.out.println("Hello");
     }
 
     private static Byte foo () {
